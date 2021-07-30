cdca5c5d30e6b80a42bfccf1a7da92bb7e66db72
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-30 09:22:27 -0600

    added map to board

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { calculateWinner } from '../helpers';
 import Board from './Board';
 
 const Game = () => {
+  const [ board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(board);
+
   const handleClick = () => {
 
   }
@@ -15,7 +20,7 @@ const Game = () => {
   }
 
   return (
-    <Board onClick={handleClick} />
+    <Board squares={board} onClick={handleClick} />
   )
   
 }
